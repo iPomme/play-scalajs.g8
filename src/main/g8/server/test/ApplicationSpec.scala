1cbff1cd0b3f833b13d08a1adb1c0453c9a01902
Nicolas Jorand <nicolas.jorand@me.com>
2018-01-21 14:33:30 +0100

    Tests correction

--- src/main/g8/server/test/ApplicationSpec.scala
+++ src/main/g8/server/test/ApplicationSpec.scala
@@ -1,27 +1,34 @@
-import org.junit.runner._
-import org.specs2.runner._
-import play.api.test._
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.libs.ws.WSClient
+import scala.concurrent.duration.DurationInt
+import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
-@RunWith(classOf[JUnitRunner])
-class ApplicationSpec() extends PlaySpecification {
+import scala.concurrent.Await
+import org.scalatest._
+import Matchers._
 
+class ApplicationSpec extends PlaySpec with GuiceOneServerPerSuite {
   "Application" should {
+    val wsClient = app.injector.instanceOf[WSClient]
+    val myPublicAddress = s"localhost:$port"
 
-    "send 404 on a bad request" in new WithApplication {
-      route(app, FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
+    "send 404 on a bad request" in {
+      val testURL = s"http://$myPublicAddress/boom"
+
+      val response = Await.result(wsClient.url(testURL).get(), 1 seconds)
+      response.status mustBe NOT_FOUND
     }
 
-    "render the index page" in new WithApplication {
-      val home = route(app, FakeRequest(GET, "/")).get
+    "render the index page" in  {
+      val testURL = s"http://$myPublicAddress/"
 
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("shouts out")
+      val response = Await.result(wsClient.url(testURL).get(), 1 seconds)
+
+      response.status mustBe OK
+      response.contentType should include ("text/html")
+      response.body should include ("shouts out")
+
     }
   }
 }
